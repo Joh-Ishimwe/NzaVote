4b32c81e52cd885e4bc71fb85eb293b4cad6621d
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-11-14 22:50:54 +0200

    inititial commit

--- utils/otp.js
+++
@@ -1,8 +0,0 @@
-
-const otpGenerator = () => {
-    // Generate a 6-digit random number as OTP
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    return otp.toString();
-};
-
-export default otpGenerator;
