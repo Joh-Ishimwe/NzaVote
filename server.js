61678d742e5007932c8b8c31e53dfd08ea1cc935
Joh-Ishimwe <j.ishimwe3@alustudent.com>
2024-11-14 23:29:37 +0200

    updating server and adding cors

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 import express from "express";
-// import bcrypt from "bcrypt";
-// import cors from "cors";
+import bcrypt from "bcrypt";
+import cors from "cors";
 import mongoose from "mongoose";
 import configurations from "./config/index.js";
-// import router from "./routes/user.routes.js"
+import router from "./routes/user.routes.js"
 import ErrorHandler from "./middleware/ErrorHandler.js"
 
 
@@ -14,11 +14,11 @@ import ErrorHandler from "./middleware/ErrorHandler.js"
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 
-// app.use('/api/v1', router);
+app.use('/api/v1', router);
 
 
 mongoose
